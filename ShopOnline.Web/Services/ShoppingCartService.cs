10fba763ca5022ca9ec96fd391b1cf9885d2e202
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-02-08 14:48:57 +0100

    added update quantity shopping cart - part 7

--- ShopOnline.Web/Services/ShoppingCartService.cs
+++ ShopOnline.Web/Services/ShoppingCartService.cs
@@ -1,4 +1,6 @@
 using System.Net.Http.Json;
+using System.Text;
+using Newtonsoft.Json;
 using ShopOnline.Models.Dtos;
 using ShopOnline.Web.Services.Contracts;
 
@@ -74,6 +76,29 @@ public class ShoppingCartService:IShoppingCartService
                 return await response.Content.ReadFromJsonAsync<CartItemDto>();
             }
             return default(CartItemDto);
+        }
+        catch (Exception e)
+        {
+            // Log Exception
+            throw;
+        }
+    }
+
+    public async Task<CartItemDto> UpdateQty(CartItemQtyUpdateDto cartItemQtyUpdateDto)
+    {
+        try
+        {
+            var jsonRequest = JsonConvert.SerializeObject(cartItemQtyUpdateDto);
+            var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json-patch+json");
+            
+            var response =  await _httpClient.PatchAsync($"api/ShoppingCart/{cartItemQtyUpdateDto.CartItemId}", content);
+            
+            if(response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<CartItemDto>();
+            }
+
+            return null;
         }
         catch (Exception e)
         {
